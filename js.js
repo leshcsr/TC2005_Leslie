5f6934893a8d3b4df30dea193e01d48bd4b52e7a
leshcsr <a01708987@tec.mx>
2023-02-16 12:19:17 -0600

    JAva Script

---
+++ js.js
@@ -0,0 +1,24 @@
+
+/* Actualmente es mas usado utilizar LET para declarar variables */ 
+var desayuno = "hotcakes";
+let comida = "alitas";
+
+/* const es para escribir constantes*/
+const precio = 120;
+
+console.log("Hola")
+console.info("No hay descuentos. El precio "+precio);
+console.warn("No le vayas a cambiar el precio");
+console.error("Que NO");
+
+/*Recibe como parametro una condicion logica, si es verdadera no hace nada, 
+si es falsa lanza un error*/
+
+console.assert(1 === 1); /*Estrictamente Igual*/
+console.assert(1 == 1); /* Igual*/
+
+console.assert(1 =="1");
+
+
+
+
