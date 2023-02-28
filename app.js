e595a64210fb71446d7569c519b35b075564daca
leshcsr <a01708987@tec.mx>
2023-02-28 12:04:38 -0600

    creé package

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-let leslie = 0;
+console.log("Hola desde ")
+
+function prom_arr(arr){
+    let p = 0;
+    let acum = 0;
+    while(p <= arr.length()){
+        acum = acum + arr[p];
+        p++;
+    }
+}
