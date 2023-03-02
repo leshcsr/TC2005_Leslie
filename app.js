54720b3f5c89910325b862075cd2302ea549a464
leshcsr <a01708987@tec.mx>
2023-03-02 00:22:30 -0600

    Lab8

--- app.js
+++ app.js
@@ -1,12 +1,71 @@
+//AQUI EMPIEZA EL LAB 8
+
+const { resolve } = require('path');
+
+//Funcion que recibe un arreglo y devuelve su promedio
+function prom_arr(arr) {
+    let p = 0;
+    let acum = 0;
+    while(p <= arr.length-1){
+        acum = acum + arr[p];
+        p++;
+    }
+    return acum/arr.length;
+}
+let arreglo = [1, 6, 2, 3, 8];
+let a = prom_arr(arreglo);
+console.log("El promedio del arreglo es: "+a)
+
+//Funcion que recibe un string y escribe el string en un archivo de texto
+function w_sring(palabra) {
+    const fs = require('fs');
+    let escribir = new Promise((resolve, reject) => {
+        fs.writeFile('index.txt',palabra, (error)=>{
+            if(error){
+                reject(error);
+            }else{
+                resolve();
+            }
+        });
+    });
+
+    escribir.then( () =>{
+        console.log("¡Se añadio la palabra con exito!")
+    })
+    .catch((error) =>{
+        console.log("Ocurrio un error: ", error)
+    })
+        
+}
+w_sring("Lab 8");
+//Ortiz Ordoñez, J. [John Ortiz Ordoñez] (02 de febrero de 2023). JavaScript - Ejercicio 313:
+//Crear un Archivo de Texto Plano usando un Objeto Promise en Node.js [Video]. Recuperado de: 
+//https://www.youtube.com/watch?v=P9YdELzuuwc
+
+
+
+/*
+function string_string(pstring){
+    
+}
+
+
+
 console.log("Hola desde npm");
-console.log("Hola desde nodemon");
+console.log("Hola desde nodemon");*/
 
 
 //Copiado del Lab11
+/*
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const { request } = require('express');
+
+const app = express();*/
+
 
 //Middleware
+/*
 app.use((request, response, next) => {
     console.log('Middleware!');
     next(); //Le permite a la petición avanzar hacia el siguiente middleware
@@ -17,27 +76,30 @@ app.use((request, response, next) => {
     response.send('¡Hola mundo!'); //Manda la respuesta
 });
 
-app.listen(3000);
+app.listen(3000);*/
 
 //Rutas
+/*
 app.use('/home', (request, response, next) => {
     response.send('Respuesta de la ruta "/Home"'); 
 });
 
-/*
+
 El profe agrego un codigo que ya tenia
 ^^^
 app.use('/pedir', (request, respose, next)=>{
 });*/
 
+//si ninguna ruta es valida
+/*
+    app.use((request, response, next) =>{
+        console.log('Otro Middleware');
+        response.status(404);
+        //Manda la respuesta
+        response.send('Ya no tenemos hot cakes');
+    })
 
 
+app.listen(3000);
 
-/*function prom_arr(arr){
-    let p = 0;
-    let acum = 0;
-    while(p <= arr.length()){
-        acum = acum + arr[p];
-        p++;
-    }
-}*/
+*/
