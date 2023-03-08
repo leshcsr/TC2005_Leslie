7332a452a638fb757a878b78010cfafb8f122816
leshcsr <a01708987@tec.mx>
2023-03-08 16:32:11 -0600

    intale paquetes

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.use('/alguna-ruta', (request, response, next) => {
+    console.log(request.body);
+});
+
+
+//Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    response.send('¡Hola mundo!'); //Manda la respuesta
+});
+
+app.listen(8080);
