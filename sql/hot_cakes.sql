cfa1895036b1fa082c80b95adee787066a599142
leshcsr <a01708987@tec.mx>
2023-03-24 10:18:54 -0600

    lab 19

--- sql/hot_cakes.sql
+++ sql/hot_cakes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 09-03-2023 a las 19:13:55
+-- Tiempo de generación: 14-03-2023 a las 18:57:28
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.0
 
@@ -42,7 +42,8 @@ CREATE TABLE `hotcakes` (
 --
 
 INSERT INTO `hotcakes` (`id`, `nombre`, `imagen`, `descripcion`, `handle`, `precio`, `created_at`) VALUES
-(1, 'Belgas', 'https://t1.uc.ltmcdn.com/es/posts/8/9/7/como_hacer_waffles_con_harina_de_hot_cakes_50798_paso_5_600.jpg', 'Hot cakes ricos', '@belgas', 150, '2023-03-09 18:12:02');
+(1, 'Belgas', 'https://t1.uc.ltmcdn.com/es/posts/8/9/7/como_hacer_waffles_con_harina_de_hot_cakes_50798_paso_5_600.jpg', 'Hot cakes ricos', '@belgas', 150, '2023-03-09 18:12:02'),
+(2, 'Avena', 'https://www.recetasnestle.com.mx/sites/default/files/srh_recipes/99c36783d5de1a8cc8ab4a1494bcf111.jpg', 'Hot cakes ricos de avena', '@avena', 150, '2023-03-09 19:09:52');
 
 -- --------------------------------------------------------
 
@@ -91,6 +92,113 @@ INSERT INTO `necesita` (`idhotcakes`, `idingredientes`, `cantidad`) VALUES
 (1, 4, NULL),
 (1, 7, NULL);
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `privilegios`
+--
+
+CREATE TABLE `privilegios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(40) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `privilegios`
+--
+
+INSERT INTO `privilegios` (`id`, `nombre`, `created_at`) VALUES
+(1, 'ver_hotcakes', '2023-03-14 17:55:35'),
+(2, 'crear_hotcakes', '2023-03-14 17:55:35');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(40) NOT NULL,
+  `descripcion` varchar(400) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id`, `nombre`, `descripcion`, `created_at`) VALUES
+(1, 'chef', '', '2023-03-14 17:55:06'),
+(2, 'influencer', '', '2023-03-14 17:55:06');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol_privilegio`
+--
+
+CREATE TABLE `rol_privilegio` (
+  `idRol` int(11) NOT NULL,
+  `idPrivilegio` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `rol_privilegio`
+--
+
+INSERT INTO `rol_privilegio` (`idRol`, `idPrivilegio`, `created_at`) VALUES
+(1, 1, '2023-03-14 17:55:53'),
+(1, 2, '2023-03-14 17:55:53'),
+(2, 1, '2023-03-14 17:56:03');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(400) NOT NULL,
+  `username` varchar(40) NOT NULL,
+  `password` varchar(400) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `username`, `password`, `created_at`) VALUES
+(1, 'Eduardo Daniel Juárez Pineda', 'lalo', 'lalolalo', '2023-03-13 18:06:24'),
+(2, 'Manu Bonito', 'A01352033', 'manubonito', '2023-03-13 18:13:19'),
+(3, 'Dante Feo', 'mike', 'richie', '2023-03-13 18:14:08'),
+(4, 'Daniel Cajas', 'cajas', '$2a$12$CxM.zJ5/kyLBoVJGZGX1zeEffz5ecRExIXCsVbDN/3H/CR4h3Whpe', '2023-03-13 18:22:59'),
+(5, 'Carlos Sandoval', 'carlitos', '$2a$12$qxs3LiMzjRF2yKIj1jtHKuQb3dUV5FnqP73ers3F6cgeNxLomXESa', '2023-03-13 18:24:01');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario_rol`
+--
+
+CREATE TABLE `usuario_rol` (
+  `idUsuario` int(11) NOT NULL,
+  `idRol` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `usuario_rol`
+--
+
+INSERT INTO `usuario_rol` (`idUsuario`, `idRol`, `created_at`) VALUES
+(4, 1, '2023-03-14 17:57:14'),
+(5, 2, '2023-03-14 17:56:35');
+
 --
 -- Índices para tablas volcadas
 --
@@ -115,6 +223,39 @@ ALTER TABLE `necesita`
   ADD KEY `idhotcakes` (`idhotcakes`);
 
 --
+-- Indices de la tabla `privilegios`
+--
+ALTER TABLE `privilegios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `rol_privilegio`
+--
+ALTER TABLE `rol_privilegio`
+  ADD PRIMARY KEY (`idRol`,`idPrivilegio`),
+  ADD KEY `idPrivilegio` (`idPrivilegio`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indices de la tabla `usuario_rol`
+--
+ALTER TABLE `usuario_rol`
+  ADD PRIMARY KEY (`idUsuario`,`idRol`),
+  ADD KEY `idRol` (`idRol`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -122,13 +263,31 @@ ALTER TABLE `necesita`
 -- AUTO_INCREMENT de la tabla `hotcakes`
 --
 ALTER TABLE `hotcakes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `ingredientes`
 --
 ALTER TABLE `ingredientes`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `privilegios`
+--
+ALTER TABLE `privilegios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
@@ -140,6 +299,20 @@ ALTER TABLE `ingredientes`
 ALTER TABLE `necesita`
   ADD CONSTRAINT `necesita_ibfk_1` FOREIGN KEY (`idhotcakes`) REFERENCES `hotcakes` (`id`),
   ADD CONSTRAINT `necesita_ibfk_2` FOREIGN KEY (`idingredientes`) REFERENCES `ingredientes` (`id`);
+
+--
+-- Filtros para la tabla `rol_privilegio`
+--
+ALTER TABLE `rol_privilegio`
+  ADD CONSTRAINT `rol_privilegio_ibfk_1` FOREIGN KEY (`idRol`) REFERENCES `roles` (`id`),
+  ADD CONSTRAINT `rol_privilegio_ibfk_2` FOREIGN KEY (`idPrivilegio`) REFERENCES `privilegios` (`id`);
+
+--
+-- Filtros para la tabla `usuario_rol`
+--
+ALTER TABLE `usuario_rol`
+  ADD CONSTRAINT `usuario_rol_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `usuario_rol_ibfk_2` FOREIGN KEY (`idRol`) REFERENCES `roles` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
