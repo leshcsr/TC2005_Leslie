b52c43be6f2917e1be2d415fca59925996f9e903
leshcsr <a01708987@tec.mx>
2023-03-24 10:22:19 -0600

    obtener privilegios

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -27,7 +27,25 @@ exports.post_login = (request, response, next) => {
                 if (doMatch) {
                     request.session.isLoggedIn = true;
                     request.session.nombre = rows[0].nombre;
-                    response.redirect('/hot_cakes/lista');
+                    User.getPrivilegios(rows[0].username)
+                    .then(([consulta_privilegios, fieldData]) => {
+                        console.log(consulta_privilegios);
+
+                        const privilegios = [];
+
+                        for (let privilegio of consulta_privilegios) {
+                            privilegios.push(privilegio.nombre);
+                        }
+
+                        console.log(privilegios);
+
+                        request.session.privilegios = privilegios;
+
+                        return request.session.save(err => {
+                            response.redirect('/hot_cakes/lista');
+                        });
+                    })
+                    .catch((error) => {console.log(error)});
                 } else {
                     request.session.mensaje = 'El usuario y/o contraseña no coinciden';
                     response.redirect('/users/login');
--- models/users.model.js
+++ models/users.model.js
@@ -30,5 +30,12 @@ module.exports = class User {
         `, [username]);
     }
 
+    static getPrivilegios(username) {
+        return db.execute(`
+            SELECT p.nombre
+            FROM privilegios p, rol_privilegio rp, roles r, usuario_rol ur, usuarios u
+            WHERE u.username = ? AND u.id = ur.idUsuario AND ur.idRol = r.id 
+                AND rp.idRol = r.id AND rp.idPrivilegio = p.id
+        `, [username]);
+            }
 }
-
